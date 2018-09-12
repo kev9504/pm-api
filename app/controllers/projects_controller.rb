572876f9c0ac20f3a55de52ac82c637ca08ee9f1
Fernando G <kev9504@gmail.com>
2018-09-12 15:43:15 +0000

    completed crud for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,44 @@
 class ProjectsController < ApplicationController
+    before_action :set_project, only: [:show, :update, :destroy]
     def index 
         @projects = Project.all 
         json_response(@projects)
+    end
+    
+    def update
+        @project.update(project_params)
+        head :no_content
+    end
+    
+    def destroy 
+        @project.destroy
+        head :no_content
+    end
+    
+    def show 
+        json_response(@project)
+    end
+    
+    def create 
+        @project = Project.create!(project_params)
+        json_response(@project, :created)
+    end
+    private
+    
+    def project_params 
+        params.permit(
+            :title, 
+            :budget, 
+            :owner, 
+            :contact_name, 
+            :contact_email, 
+            :contact_phone,
+            :status,
+            :description
+            )
+    end
+    
+    def set_project
+        @project = Project.find(params[:id])
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+PROJECTS = []
+2.times do 
+    project = {
+        :title => Faker::Space.launch_vehicule,
+        :budget => rand(100..1000),
+        :owner => rand(0..5),
+        :delivery_date => Faker::Date.forward(rand(10..20)),
+        :contact_name => Faker::Name.name,
+        :contact_email => Faker::Internet.email,
+        :contact_phone => (Faker::PhoneNumber.subscriber_number + rand(100..300).to_s).to_i,
+        :status => rand(0..6),
+        :description => Faker::HitchhikersGuideToTheGalaxy.quote
+    }
+    PROJECTS << project
+end
+
+TASKS = []
+4.times do 
+    task ={
+        body: "add #{Faker::Measurement.metric_weight} of #{Faker::Science.element}",
+        source: Faker::ProgrammingLanguage.name,
+        status: rand(0..2),
+        difficulty: rand(0..10)
+    }
+    TASKS << task
+end
+
+
+PROJECTS.each do |project|
+    @project = Project.create!(project)
+    puts 'project created' if @project
+end
+
+TASKS.each do |task|
+    @task = Project.find(rand(1..2)).tasks.create!(task)
+    puts 'task created' if @task
+end
