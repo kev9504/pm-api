82bb048b8b2f0a186ee595f69db0e098c433b3c9
Fernando G <kev9504@gmail.com>
2018-09-12 16:15:07 +0000

    modified dependency

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it {should have_many(:tasks)}
+  it {should have_many(:tasks).dependent(:destroy)}
   it {should validate_presence_of(:title)}
   it {should validate_presence_of(:owner)}
 end
