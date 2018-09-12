47e853143888b88111998eb2f2dd4c9670ed30e4
Fernando G <kev9504@gmail.com>
2018-09-12 23:38:20 +0000

    compmleted CRUD for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,46 @@
 class TasksController < ApplicationController
+    before_action :set_project, only:[:index, :create]
+    before_action :set_task, only:[:show, :update, :destroy]
+    
+    def index 
+        json_response(@project.tasks)
+    end
+    
+    def show 
+        json_response(@task)
+    end
+    
+    def create
+        @task = @project.tasks.create!(task_params)
+        json_response(@task)
+    end
+    
+    def update 
+        @task.update(task_params)
+        head :no_content
+    end
+    
+    def destroy 
+        @task.destroy
+        head :no_content
+    end
+    
+    private
+    
+    def set_project 
+        @project = Project.find(params[:project_id])
+    end
+    
+    def set_task
+        @task = Task.find(params[:id])
+    end
+    
+    def task_params
+        params.permit(
+            :body, 
+            :source, 
+            :status, 
+            :difficulty,
+            )
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :projects do 
-      :tasks
+      resources :tasks
   end
 end
