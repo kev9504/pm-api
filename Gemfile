3777bcdb300b243f7cd9c0cfb5e73bc0a8d26c1c
Fernando G <kev9504@gmail.com>
2018-09-19 23:00:07 +0000

    added rack-cors

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.11'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bcrypt'
-
+gem 'rack-cors', :require => 'rack/cors'
 group :development, :test do
   gem 'rspec-rails'
   gem 'faker'
--- spec/factories/tasks.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :task do
-    project_id { rand(0..2)}
-    body { "add #{Faker::Measurement.metric_weight} of #{Faker::Science.element}" }
-    source { Faker::ProgrammingLanguage.name }
-    status { rand(0..2)}
-    difficulty { rand(0..10) }
-  end
-end
